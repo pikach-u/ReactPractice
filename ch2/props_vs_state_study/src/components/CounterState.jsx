d286b2dab1d7f4456efa42f744de4d84a8e85c76
pikach-u <ziwon05@naver.com>
2025-07-30 14:41:08 +0900

    tailwind project 생성

--- ch2/props_vs_state_study/src/components/CounterState.jsx
+++ ch2/props_vs_state_study/src/components/CounterState.jsx
@@ -8,7 +8,7 @@ const CounterState = () => {
       <h2>State 기반 카운터</h2>
       <p>카운트: {count}</p>
       <button onClick={() => setCount(count + 1)}>1 증가</button>
-      <button onClick={() => setCount(count - 1)}>1 증가</button>
+      <button onClick={() => setCount(count - 1)}>1 감소</button>
     </div>
   );
 };
