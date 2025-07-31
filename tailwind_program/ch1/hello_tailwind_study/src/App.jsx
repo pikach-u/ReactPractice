6928d0ffd5e04afcbce644893ade3ea679958001
pikach-u <ziwon05@naver.com>
2025-07-31 12:49:44 +0900

    Calendar 실습

--- tailwind_program/ch1/hello_tailwind_study/src/App.jsx
+++ tailwind_program/ch1/hello_tailwind_study/src/App.jsx
@@ -1,5 +1,11 @@
+import Calendar from "./components/Calendar";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="bg-red-100 min-h-screen flex justify-center items-center">
+      <Calendar />
+    </div>
+  );
 };
 
 export default App;
