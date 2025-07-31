5de92e10881df08a2aa8c346276fdee4be7d3baf
pikach-u <ziwon05@naver.com>
2025-07-31 15:25:47 +0900

    1. 컴포넌트가 렌더링 될때 2. App 컴포넌트 마운트 될 때

--- tailwind_program/ch3/use-effect-study/src/App.jsx
+++ tailwind_program/ch3/use-effect-study/src/App.jsx
@@ -4,21 +4,29 @@ const App = () => {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    console.log("1. 화면이 렌더링 될때마다.");
+    console.log("1. App 컴포넌트가 렌더링 될때마다.");
   });
 
+  useEffect(() => {
+    console.log("2. App 컴포넌트 마운트 될 때 (App 컴포넌트 처음 불러왔을 때)");
+  }, []);
+
+  useEffect(() => {
+    console.log("3. App 컴포넌트 마운트 될 때 + count 값에 변화가 발생할 때");
+  }, [count]);
+
   return (
-    <div className="flex flex-col justify-center items-center gap-20">
-      <p>{count}</p>
-      <div className="flex gap-12">
+    <div className="min-h-screen flex flex-col justify-center items-center gap-12">
+      <div className="flex items-center gap-2">
+        <p className="mr-2">{count}</p>
         <button
-          className="py-4 px-8 rounded-xl bg-red-300"
+          className="py-1 px-2 rounded bg-red-300"
           onClick={() => setCount(count + 1)}
         >
           Increase
         </button>
         <button
-          className="py-4 px-8 rounded-xl bg-blue-300"
+          className="py-1 px-2 rounded bg-blue-300"
           onClick={() => setCount(count - 1)}
         >
           Decrease
