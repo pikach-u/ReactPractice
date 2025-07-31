6b0c268434bbe666baa4a06871ed1f5ccb5afd98
pikach-u <ziwon05@naver.com>
2025-07-31 13:08:14 +0900

    UseState 실습과정

---
+++ tailwind_program/ch3/use-state-study/src/App.jsx
@@ -0,0 +1,11 @@
+import ItemAdd from "./components/ItemAdd";
+
+const App = () => {
+  return (
+    <div className="bg-red-100 min-h-screen flex justify-center items-center">
+      <ItemAdd />
+    </div>
+  );
+};
+
+export default App;
