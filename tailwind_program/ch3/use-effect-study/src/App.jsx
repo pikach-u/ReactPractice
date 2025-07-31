07347eb409d0354111a39c2c3a08051e6f1066ff
pikach-u <ziwon05@naver.com>
2025-07-31 15:38:47 +0900

    Counter로 분리

--- tailwind_program/ch3/use-effect-study/src/App.jsx
+++ tailwind_program/ch3/use-effect-study/src/App.jsx
@@ -1,37 +1,9 @@
-import { useEffect, useState } from "react";
+import Counter from "./components/Counter";
 
 const App = () => {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    console.log("1. App 컴포넌트가 렌더링 될때마다.");
-  });
-
-  useEffect(() => {
-    console.log("2. App 컴포넌트 마운트 될 때 (App 컴포넌트 처음 불러왔을 때)");
-  }, []);
-
-  useEffect(() => {
-    console.log("3. App 컴포넌트 마운트 될 때 + count 값에 변화가 발생할 때");
-  }, [count]);
-
   return (
     <div className="min-h-screen flex flex-col justify-center items-center gap-12">
-      <div className="flex items-center gap-2">
-        <p className="mr-2">{count}</p>
-        <button
-          className="py-1 px-2 rounded bg-red-300"
-          onClick={() => setCount(count + 1)}
-        >
-          Increase
-        </button>
-        <button
-          className="py-1 px-2 rounded bg-blue-300"
-          onClick={() => setCount(count - 1)}
-        >
-          Decrease
-        </button>
-      </div>
+      <Counter />
     </div>
   );
 };
---
+++ tailwind_program/ch3/use-effect-study/src/components/Counter.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("1. Counter 컴포넌트가 렌더링 될때마다.");
+  });
+
+  useEffect(() => {
+    console.log(
+      "2. Counter 컴포넌트 마운트 될 때 (Counter 컴포넌트 처음 불러왔을 때)"
+    );
+  }, []);
+
+  useEffect(() => {
+    console.log(
+      "3. Counter 컴포넌트 마운트 될 때 + count 값에 변화가 발생할 때"
+    );
+  }, [count]);
+
+  return (
+    <div className="flex items-center gap-2">
+      <p className="mr-2">Count: {count}</p>
+      <button
+        className="py-1 px-2 rounded bg-red-300"
+        onClick={() => setCount(count + 1)}
+      >
+        Increase
+      </button>
+      <button
+        className="py-1 px-2 rounded bg-blue-300"
+        onClick={() => setCount(count - 1)}
+      >
+        Decrease
+      </button>
+    </div>
+  );
+};
+
+export default Counter;
