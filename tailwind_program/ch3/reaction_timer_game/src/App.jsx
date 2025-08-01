4db01dde22072a7df306824609ad62e5b5454a11
pikach-u <ziwon05@naver.com>
2025-08-01 11:19:09 +0900

    interval을 변수로 등록 clearInterval

--- tailwind_program/ch3/reaction_timer_game/src/App.jsx
+++ tailwind_program/ch3/reaction_timer_game/src/App.jsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import ReactionTimerGame from "./components/ReactionTimerGame";
 
 const App = () => {
+  const [isShow, setIsShow] = useState(false);
   return (
     <div className="bg-green-100 min-h-screen flex justify-center items-center">
-      <ReactionTimerGame />
+      {isShow && <ReactionTimerGame />}
+      <button onClick={() => setIsShow(!isShow)}>button</button>
     </div>
   );
 };
--- tailwind_program/ch3/reaction_timer_game/src/components/ReactionTimerGame.jsx
+++ tailwind_program/ch3/reaction_timer_game/src/components/ReactionTimerGame.jsx
@@ -2,13 +2,13 @@ import { useEffect } from "react";
 
 const ReactionTimerGame = () => {
   useEffect(() => {
-    setTimeout(() => {
-      console.log("setTimeout");
-    }, 2000);
-    setInterval(() => {
-      console.log("setInterval");
-    }, 3000);
+    const interval = setInterval(() => {
+      console.log("setInverval");
+    }, 1000);
+
+    return () => clearInterval(interval);
   }, []);
+
   return (
     <div className="flex flex-col justify-center items-center gab-8 p-8 bg-white shadow-xl">
       <h1 className="text-2xl font-bold">⏱️ 10초 반응 게임</h1>
