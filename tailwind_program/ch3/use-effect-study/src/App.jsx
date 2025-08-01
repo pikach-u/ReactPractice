a293135ab16959120cbd0b5a8faa9cf6ae458561
pikach-u <ziwon05@naver.com>
2025-08-01 09:34:25 +0900

    React Boilerplate

--- tailwind_program/ch3/use-effect-study/src/App.jsx
+++ tailwind_program/ch3/use-effect-study/src/App.jsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
 import Counter from "./components/Counter";
 
 const App = () => {
+  const [isShow, setIsShow] = useState(false);
   return (
     <div className="min-h-screen flex flex-col justify-center items-center gap-12">
-      <Counter />
+      {isShow && <Counter />}
+      <button onClick={() => setIsShow(!isShow)}>
+        {isShow ? "카운터 비활성화" : "활성화"}
+      </button>
     </div>
   );
 };
--- tailwind_program/ch3/use-effect-study/src/components/Counter.jsx
+++ tailwind_program/ch3/use-effect-study/src/components/Counter.jsx
@@ -4,19 +4,21 @@ const Counter = () => {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    console.log("1. Counter 컴포넌트가 렌더링 될때마다.");
+    console.log("1. Counter 컴포넌트가 렌더링 될때마다");
   });
 
   useEffect(() => {
     console.log(
-      "2. Counter 컴포넌트 마운트 될 때 (Counter 컴포넌트 처음 불러왔을 때)"
+      "2. Counter 컴포넌트 마운트 될 때 (Counter 컴포넌트를 처음 불러왔을 때)"
     );
   }, []);
 
   useEffect(() => {
-    console.log(
-      "3. Counter 컴포넌트 마운트 될 때 + count 값에 변화가 발생할 때"
-    );
+    console.log("3. count 값에 변화가 발생할 때");
+
+    return () => {
+      console.log("4. Counter 컴포넌트가 언마운트 될 때 (삭제될 때)");
+    };
   }, [count]);
 
   return (
