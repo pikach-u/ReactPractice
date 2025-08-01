bb7128be01c7877296ea0a3e03da465a465e2f97
pikach-u <ziwon05@naver.com>
2025-08-01 15:12:54 +0900

    textbox util

--- tailwind_program/ch1/utilities-class-study/src/App.jsx
+++ tailwind_program/ch1/utilities-class-study/src/App.jsx
@@ -1,12 +1,13 @@
 const App = () => {
   return (
-    <div className="bg-red-100 min-h-screen flex flex-col justify-center items-center gap-10">
-      <button className="bg-blue-500 hover:bg-blue-700 active:bg-blue-900 text-white font-semibold py-2 px-4 rounded">
-        파란 버튼
-      </button>
-      <button className="bg-gray-200 hover:bg-gray-300 active:bg-gray-500 text-gray-800 font-medium py-2 px-4 rounded-lg border">
-        회색 버튼
-      </button>
+    <div className="bg-red-100 min-h-screen flex flex-col justify-center items-center gap-8">
+      <div className="max-w-sm mx-auto bg-white rounded-xl shadow-md overflow-hidden p-6 mt-8">
+        <h2 className="text-xl font-semibold text-gray-800">제목입니다</h2>
+        <p className="mt-2 text-gray-600 text-sm">
+          안녕하세요 여기에 간단한 설명이 들어갑니다. 그림자와 간격을
+          조절해보세요.
+        </p>
+      </div>
     </div>
   );
 };
