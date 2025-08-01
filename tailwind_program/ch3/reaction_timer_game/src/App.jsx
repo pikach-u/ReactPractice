eaafdf25f2e656f4cbbc768ed9f2cae6b874f161
pikach-u <ziwon05@naver.com>
2025-08-01 11:34:05 +0900

    isRunning으로 state 관리

--- tailwind_program/ch3/reaction_timer_game/src/App.jsx
+++ tailwind_program/ch3/reaction_timer_game/src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from "react";
 import ReactionTimerGame from "./components/ReactionTimerGame";
 
 const App = () => {
-  const [isShow, setIsShow] = useState(false);
   return (
     <div className="bg-green-100 min-h-screen flex justify-center items-center">
-      {isShow && <ReactionTimerGame />}
-      <button onClick={() => setIsShow(!isShow)}>button</button>
+      <ReactionTimerGame />
     </div>
   );
 };
--- tailwind_program/ch3/reaction_timer_game/src/components/ReactionTimerGame.jsx
+++ tailwind_program/ch3/reaction_timer_game/src/components/ReactionTimerGame.jsx
@@ -1,17 +1,34 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const ReactionTimerGame = () => {
-  useEffect(() => {
-    const interval = setInterval(() => {
-      console.log("setInverval");
-    }, 1000);
+  const [isRunning, setIsRunning] = useState(false);
 
-    return () => clearInterval(interval);
-  }, []);
+  const handleStart = () => {
+    setIsRunning(true);
+  };
+
+  useEffect(() => {
+    if (!isRunning) return;
+
+    const timer = setTimeout(() => {
+      console.log("10초 지남");
+    }, 10000);
+
+    return () => clearTimeout(timer);
+  }, [isRunning]);
 
   return (
     <div className="flex flex-col justify-center items-center gab-8 p-8 bg-white shadow-xl">
       <h1 className="text-2xl font-bold">⏱️ 10초 반응 게임</h1>
+
+      {!isRunning && (
+        <button
+          className="bg-blue=500 text-white px-6 py-3 rounded-lg hover:bg-blue-600"
+          onClick={handleStart}
+        >
+          시작
+        </button>
+      )}
     </div>
   );
 };
