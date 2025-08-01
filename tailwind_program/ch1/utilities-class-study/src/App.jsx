b40590f26f614e08e4949a08dc8e9e3a79e6e549
pikach-u <ziwon05@naver.com>
2025-08-01 14:44:41 +0900

    button 관련 util

--- tailwind_program/ch1/utilities-class-study/src/App.jsx
+++ tailwind_program/ch1/utilities-class-study/src/App.jsx
@@ -1,8 +1,12 @@
 const App = () => {
   return (
     <div className="bg-red-100 min-h-screen flex flex-col justify-center items-center">
-      <button className="border-2">버튼</button>
-      <button className="border-2 m-20 p-20">버튼</button>
+      <button
+        className="border border-purple-400 rounded-xl px-4 py-2 
+      bg-white shadow-xl shadow-yellow-500 hover:bg-purple-300 active:bg-purple-500"
+      >
+        버튼
+      </button>
     </div>
   );
 };
