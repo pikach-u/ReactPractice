5d563a099e8715e6e9ed32ce0cf589381de0687e
pikach-u <ziwon05@naver.com>
2025-08-01 14:28:01 +0900

    padding / margin

--- tailwind_program/ch1/utilities-class-study/src/App.jsx
+++ tailwind_program/ch1/utilities-class-study/src/App.jsx
@@ -1,5 +1,10 @@
 const App = () => {
-  return <div className="bg-red-100 min-h-screen flex">App</div>;
+  return (
+    <div className="bg-red-100 min-h-screen flex flex-col justify-center items-center">
+      <button className="border-2">버튼</button>
+      <button className="border-2 m-20 p-20">버튼</button>
+    </div>
+  );
 };
 
 export default App;
