ac9787d78974e7fc90e6c65ccc158e91e6274744
pikach-u <ziwon05@naver.com>
2025-08-04 13:43:08 +0900

    이미지에 반응형 옵션 적용

--- tailwind_program/ch2/responsive-study/src/App.jsx
+++ tailwind_program/ch2/responsive-study/src/App.jsx
@@ -1,9 +1,9 @@
-import VisibilityToggle from "./components/VisibilityToggle";
+import ResponsiveImage from "./components/ResponsiveImage";
 
 const App = () => {
   return (
     <div className="bg-red-100 min-h-screen flex justify-center items-center">
-      <VisibilityToggle />
+      <ResponsiveImage />
     </div>
   );
 };
---
+++ tailwind_program/ch2/responsive-study/src/components/ResponsiveImage.jsx
@@ -0,0 +1,13 @@
+const ResponsiveImage = () => {
+  return (
+    <div className="p-6 text-center bg-yellow-300">
+      <img
+        className="w-40 sm:w-60 md:w-80"
+        src="https://upload.wikimedia.org/wikipedia/ko/a/a6/Pok%C3%A9mon_Pikachu_art.png"
+        alt="샘플 이미지"
+      />
+    </div>
+  );
+};
+
+export default ResponsiveImage;
