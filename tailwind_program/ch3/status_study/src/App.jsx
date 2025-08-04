ba76727e28be23088d5b9b632135dc6651698d5f
pikach-u <ziwon05@naver.com>
2025-08-04 14:47:13 +0900

    카드에 group-hover 옵션 추가

--- tailwind_program/ch3/status_study/src/App.jsx
+++ tailwind_program/ch3/status_study/src/App.jsx
@@ -1,12 +1,13 @@
 import FocusInput from "./components/FocusInput";
 import FullStateButton from "./components/FullStateButton";
+import GroupHoverCard from "./components/GroupHoverCard";
 import HoverButton from "./components/HoverButton";
 import PressEffectButton from "./components/PressEffectButton";
 
 const App = () => {
   return (
     <div className="bg-red-100 min-h-screen flex justify-center items-center">
-      <FullStateButton />
+      <GroupHoverCard />
     </div>
   );
 };
