eb41d0834d4a97da135c533850d6550430fe66d9
pikach-u <ziwon05@naver.com>
2025-08-04 14:37:25 +0900

    버튼에 disabled 옵션 추가

--- tailwind_program/ch3/status_study/src/App.jsx
+++ tailwind_program/ch3/status_study/src/App.jsx
@@ -1,11 +1,12 @@
 import FocusInput from "./components/FocusInput";
+import FullStateButton from "./components/FullStateButton";
 import HoverButton from "./components/HoverButton";
 import PressEffectButton from "./components/PressEffectButton";
 
 const App = () => {
   return (
     <div className="bg-red-100 min-h-screen flex justify-center items-center">
-      <PressEffectButton />
+      <FullStateButton />
     </div>
   );
 };
---
+++ tailwind_program/ch3/status_study/src/components/GroupHoverCard.jsx
@@ -0,0 +1,11 @@
+const GroupHoverCard = () => {
+  return (
+    <div className="group p-4">
+      <h3>제목</h3>
+      <p>설명을 여기에 입력하세요.</p>
+      <div>⭐ 즐겨찾기</div>
+    </div>
+  );
+};
+
+export default GroupHoverCard;
