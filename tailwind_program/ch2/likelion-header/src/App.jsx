4890e0d00b08e5d50c6a86681614e17bc0274999
pikach-u <ziwon05@naver.com>
2025-08-04 10:18:56 +0900

    강의 배너 텍스트 추가

--- tailwind_program/ch2/likelion-header/src/App.jsx
+++ tailwind_program/ch2/likelion-header/src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Lectures from "./components/Lectures";
 
 const App = () => {
   return (
     <div className="bg-red-100 min-h-screen">
       <Header />
+      <Lectures />
     </div>
   );
 };
--- tailwind_program/ch2/likelion-header/src/components/Lectures.jsx
+++ tailwind_program/ch2/likelion-header/src/components/Lectures.jsx
@@ -1,5 +1,18 @@
 const Lectures = () => {
-  return <div className="bg-green-100">Lectures</div>;
+  return (
+    <ul className="bg-green-100">
+      <li className="text-[#222222]">
+        <img
+          className="w-[290px] h-[194px] object-cover rounded-xl"
+          src="1.webp"
+          alt="1"
+        />
+        <h3>
+          일은 간편해지고 수익은 성장하는 자동화 스킬 : 파이썬부터 Chat GPT 까지
+        </h3>
+      </li>
+    </ul>
+  );
 };
 
 export default Lectures;
