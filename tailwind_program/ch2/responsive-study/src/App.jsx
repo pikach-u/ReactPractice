eef4410c0f8a906513826bdfd6ef9c1c7be67953
pikach-u <ziwon05@naver.com>
2025-08-04 13:04:16 +0900

    first commit

---
+++ tailwind_program/ch2/responsive-study/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="bg-red-100 p-6 text-center">
+      <p className="text-base sm:text-lg md:text-xl lg:text-2xl text-blue-700 font-bold">
+        반응형 텍스트 크기입니다!
+      </p>
+    </div>
+  );
+};
+
+export default App;
