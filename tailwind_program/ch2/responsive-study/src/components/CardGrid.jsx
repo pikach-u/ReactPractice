73e93ccb0eefff4a6dcba87395b84713588306b4
pikach-u <ziwon05@naver.com>
2025-08-04 13:23:40 +0900

    카드에 반응형 옵션 추가

--- tailwind_program/ch2/responsive-study/src/components/CardGrid.jsx
+++ tailwind_program/ch2/responsive-study/src/components/CardGrid.jsx
@@ -1,8 +1,13 @@
 const CardGrid = () => {
   return (
-    <ul>
+    <ul className="p-20 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
       {Array.from({ length: 66 }).map((_, i) => (
-        <li>카드 {i + 1}</li>
+        <li
+          key={i}
+          className="bg-yellow-100 p-6 rounded shadow-2xl text-center"
+        >
+          카드 {i + 1}
+        </li>
       ))}
     </ul>
   );
