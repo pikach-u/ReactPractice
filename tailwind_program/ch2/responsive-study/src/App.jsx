8c5504ca0891bf818d699c699b4a4f2053d24fb5
pikach-u <ziwon05@naver.com>
2025-08-04 13:08:57 +0900

    반응형 텍스트

--- tailwind_program/ch2/responsive-study/src/App.jsx
+++ tailwind_program/ch2/responsive-study/src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <div className="bg-red-100 p-6 text-center">
-      <p className="text-base sm:text-lg md:text-xl lg:text-2xl text-blue-700 font-bold">
+      <p className="text-base sm:text-lg md:text-xl lg:text-2xl text-blue-700 sm:text-green-700 md:text-yellow-700 lg:text-pink-700 font-bold">
         반응형 텍스트 크기입니다!
       </p>
     </div>
