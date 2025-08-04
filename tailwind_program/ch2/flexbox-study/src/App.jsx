a6bbdaffd22a42de8e7f3fd21d48fe3bdcb58ff4
pikach-u <ziwon05@naver.com>
2025-08-04 09:40:38 +0900

    flex justify-between 적용

--- tailwind_program/ch2/flexbox-study/src/App.jsx
+++ tailwind_program/ch2/flexbox-study/src/App.jsx
@@ -1,7 +1,9 @@
 const App = () => {
   return (
-    <div className="bg-red-100">
-      <div className="w-1/3 bg-blue-100 rounded">카드 1</div>
+    <div className="bg-red-100 flex justify-between p-6 gap-4">
+      <div className="w-1/3 p-4 bg-blue-100 rounded">카드 1</div>
+      <div className="w-1/3 p-4 bg-green-100 rounded">카드 2</div>
+      <div className="w-1/3 p-4 bg-yellow-100 rounded">카드 3</div>
     </div>
   );
 };
