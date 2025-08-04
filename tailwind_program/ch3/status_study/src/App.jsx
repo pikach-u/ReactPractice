72cdcc0f1934e92aa87edfa2b8a3c3b91c0f1fd5
pikach-u <ziwon05@naver.com>
2025-08-04 14:26:27 +0900

    버튼 클릭 시 transform scale 효과 추가

--- tailwind_program/ch3/status_study/src/App.jsx
+++ tailwind_program/ch3/status_study/src/App.jsx
@@ -1,10 +1,11 @@
 import FocusInput from "./components/FocusInput";
 import HoverButton from "./components/HoverButton";
+import PressEffectButton from "./components/PressEffectButton";
 
 const App = () => {
   return (
     <div className="bg-red-100 min-h-screen flex justify-center items-center">
-      <FocusInput />
+      <PressEffectButton />
     </div>
   );
 };
