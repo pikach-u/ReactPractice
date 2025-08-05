d08ca22b884a7c469679a3b4250f2fbd69f4e7c8
pikach-u <ziwon05@naver.com>
2025-08-05 13:19:55 +0900

    사용자 목록 name 추가 구현

--- tailwind_program/ch4/conditional-login-study/src/components/UserList.jsx
+++ tailwind_program/ch4/conditional-login-study/src/components/UserList.jsx
@@ -1,19 +1,27 @@
 import { useState } from "react";
 
 const UserList = () => {
-  const [users, setUsers] = useState([
-    { id: 1, name: "pikachu" },
-    { id: 2, name: "피카츄" },
-  ]);
-
+  const [users, setUsers] = useState(["pikachu", "피카츄"]);
   const [name, setName] = useState("");
 
+  const addUser = () => {
+    if (name.trim()) {
+      setUsers([...users, name]);
+      setName("");
+    }
+  };
   return (
     <div>
       <h2>사용자 목록</h2>
+      <input
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="이름 입력"
+      />
+      <button onClick={addUser}>추가</button>
       <ul>
         {users.map((user, index) => (
-          <li key={index}>{user.name}</li>
+          <li key={index}>{user}</li>
         ))}
       </ul>
     </div>
