7b9912deb656fb3e99a6147d4500596f572b8e9e
pikach-u <ziwon05@naver.com>
2025-08-05 15:45:16 +0900

    상태 끌어올리기

--- tailwind_program/ch4/state-up-study/src/components/C.jsx
+++ tailwind_program/ch4/state-up-study/src/components/C.jsx
@@ -1,5 +1,5 @@
-const C = () => {
-  return <div className="bg-green-400 m-12 p-12">C</div>;
+const C = ({ count }) => {
+  return <div className="bg-green-400 m-12 p-12">C {count}</div>;
 };
 
 export default C;
