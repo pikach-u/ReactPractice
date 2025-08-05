04d99f7b1f21ac408cdc5fc2ab55be22c441921e
pikach-u <ziwon05@naver.com>
2025-08-05 15:12:24 +0900

    await 방식으로 수정

--- tailwind_program/ch4/divide-component/src/components/ContainerComponent.jsx
+++ tailwind_program/ch4/divide-component/src/components/ContainerComponent.jsx
@@ -1,18 +1,31 @@
 import { useEffect, useState } from "react";
+import UserList from "./UserList";
 
-//1. promise/then
-//2. async/await
+// 1. promise/then
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then((res) => res.json())
+//   .then((data) => setUsers(data));
+// 2. async/await
 
 const ContainerComponent = () => {
-  const [user, setUsers] = useState([]);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => console.log(users), [users]);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => res.json())
-      .then((data) => console.log(data));
+    const fetchUsers = async () => {
+      try {
+        const res = await fetch("https://jsonplaceholder.typicode.com/users");
+        console.log(res);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchUsers();
   }, []);
 
-  return <div>ContainerComponent</div>;
+  return <UserList users={users} />;
 };
 
 export default ContainerComponent;
