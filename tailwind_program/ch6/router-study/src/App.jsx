4d55a63e7cdd99b8eb4e0d9c434c3ba65450c4c8
pikach-u <ziwon05@naver.com>
2025-08-07 15:38:09 +0900

    About 페이지

--- tailwind_program/ch6/router-study/src/App.jsx
+++ tailwind_program/ch6/router-study/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
+import About from "./pages/about";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   );
--- tailwind_program/ch6/router-study/src/pages/home.jsx
+++ tailwind_program/ch6/router-study/src/pages/home.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
-    <div className="bg-red-100 min-h-screen flex justify-center items-center">
+    <div className="bg-red-100 min-h-screen flex flex-col gap-8 justify-center items-center">
       Home
+      <Link className="bg-blue-300 px-4 py-2 rounded" to="/about">
+        About 페이지 이동
+      </Link>
     </div>
   );
 };
