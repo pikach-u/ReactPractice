dc2f804ebb6ec2e66a4e3c36f403d76f9e6b5b37
pikach-u <ziwon05@naver.com>
2025-08-07 11:12:11 +0900

    Counter

--- tailwind_program/ch5/zustand-middleware-study/src/components/Counter.jsx
+++ tailwind_program/ch5/zustand-middleware-study/src/components/Counter.jsx
@@ -1,7 +1,19 @@
+import { useEffect } from "react";
 import useCounterStore from "../store/counterStore";
 
 const Counter = () => {
   const { count, increase, decrease } = useCounterStore();
+
+  useEffect(() => {
+    const unsub = useCounterStore.subscribe(
+      (state) => state.count,
+      (count) => {
+        console.log("카운트가 변경되었습니다:", count);
+      }
+    );
+
+    return () => unsub();
+  }, []);
 
   return (
     <div>
--- tailwind_program/ch5/zustand-middleware-study/src/store/counterStore.js
+++ tailwind_program/ch5/zustand-middleware-study/src/store/counterStore.js
@@ -1,9 +1,12 @@
 import { create } from "zustand";
+import { subscribeWithSelector } from "zustand/middleware";
 
-const useCounterStore = create((set) => ({
-  count: 0,
-  increase: () => set((state) => ({ count: state.count + 1 })),
-  decrease: () => set((state) => ({ count: state.count - 1 })),
-}));
+const useCounterStore = create(
+  subscribeWithSelector((set) => ({
+    count: 0,
+    increase: () => set((state) => ({ count: state.count + 1 })),
+    decrease: () => set((state) => ({ count: state.count - 1 })),
+  }))
+);
 
 export default useCounterStore;
