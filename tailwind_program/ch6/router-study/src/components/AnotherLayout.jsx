a04948285148ca47943380cceb45e9ac8b3733df
pikach-u <ziwon05@naver.com>
2025-08-07 15:46:44 +0900

    branch 생성 후 새로운 레이아웃 생성

---
+++ tailwind_program/ch6/router-study/src/components/AnotherLayout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import AnotherHeader from "./AnotherHeader";
+
+const AnotherLayout = () => {
+  return (
+    <>
+      <AnotherHeader />
+      <Outlet />
+    </>
+  );
+};
+
+export default AnotherLayout;
