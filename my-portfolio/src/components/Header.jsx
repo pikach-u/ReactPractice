490428b735ab083d46f03ec9d0b39ddf63e08a7e
pikach-u <ziwon05@naver.com>
2025-08-11 15:39:32 +0900

    버튼 추가 및 연결

--- my-portfolio/src/components/Header.jsx
+++ my-portfolio/src/components/Header.jsx
@@ -1,12 +1,13 @@
 import { Button } from "@radix-ui/themes";
 import { useEffect } from "react";
+import { FaGithubAlt, FaBlogger, FaLinkedin, FaCat } from "react-icons/fa";
 
 const Header = ({ profile }) => {
   useEffect(() => console.log(profile), []);
 
   return (
-    <header className="bg-green-100 min-h-screen flex flex-col justify-center items-center">
-      <div className="bg-pink-100 max-w-4xl mx-auto">
+    <header className="bg-yellow-100 min-h-screen flex flex-col justify-center items-center">
+      <div className="bg-blue-100 max-w-4xl mx-auto">
         <h1 className="text-6xl font-bold mb-4 animate-fade-in">
           {profile.name}
         </h1>
@@ -25,7 +26,44 @@ const Header = ({ profile }) => {
 
         <div>
           <Button asChild variant="outline">
-            <a>GitHub</a>
+            <a
+              href={profile.links.github_url}
+              target="_blank"
+              rel="nooper noreferrer"
+            >
+              <FaGithubAlt className="w-4 h-4 mr-2" />
+              GitHub
+            </a>
+          </Button>
+          <Button asChild variant="outline">
+            <a
+              href={profile.links.blog_url}
+              target="_blank"
+              rel="nooper noreferrer"
+            >
+              <FaBlogger className="w-4 h-4 mr-2" />
+              Blog
+            </a>
+          </Button>
+          <Button asChild variant="outline">
+            <a
+              href={profile.links.linkedin_url}
+              target="_blank"
+              rel="nooper noreferrer"
+            >
+              <FaLinkedin className="w-4 h-4 mr-2" />
+              Linkedin
+            </a>
+          </Button>
+          <Button asChild>
+            <a
+              href={`mailto:${profile.email}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaCat className="w-4 h-4 mr-2" />
+              Contact
+            </a>
           </Button>
         </div>
       </div>
