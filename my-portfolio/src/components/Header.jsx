80ef9179fbda7c3bdd40d9713fb7b9de98c9ee6e
pikach-u <ziwon05@naver.com>
2025-08-11 14:51:18 +0900

    정보를 위한 json 예시 데이터 추가 헤더 작성

---
+++ my-portfolio/src/components/Header.jsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+
+const Header = ({ profile }) => {
+  useEffect(() => console.log(profile), []);
+
+  return (
+    <header className="bg-green-100 min-h-screen flex flex-col justify-center items-center">
+      <div className="bg-pink-100 max-w-4xl mx-auto">
+        <h1 className="text-6xl font-bold mb-4 animate-fade-in">
+          {profile.name}
+        </h1>
+        <h2
+          className="text-2xl text-gray-600 mb-6 animate-fade-up"
+          style={{ animationDelay: "0.2s" }}
+        >
+          {profile.title}
+        </h2>
+        <p
+          className="text-lg mb-8 text-gray-500 max-w-2xl mx-auto animate-fade-up"
+          style={{ animationDelay: "0.4s" }}
+        >
+          {profile.one_liner}
+        </p>
+
+        <div></div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
